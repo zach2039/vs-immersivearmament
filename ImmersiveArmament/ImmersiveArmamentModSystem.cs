5dfa62a8d9b6dcdf93765e7b9206e3d1a10f318f
Zach2039 <16394465+zach2039@users.noreply.github.com>
2025-03-09 01:47:47 -0600

    Initial commit

---
+++ ImmersiveArmament/ImmersiveArmamentModSystem.cs
@@ -0,0 +1,26 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace ImmersiveArmament
+{
+    public class ImmersiveArmamentModSystem : ModSystem
+    {
+        // Called on server and client
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + Lang.Get("mymodid:hello"));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side");
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side");
+        }
+    }
+}
